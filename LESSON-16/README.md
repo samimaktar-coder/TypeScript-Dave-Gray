e044881c5f99dba4f8d2f4272e900591447b2e41
Samim Aktar <samimaktar94320@gmail.com>
2024-02-25 14:15:16 +0530

    cart app part-1 complete

---
+++ LESSON-16/README.md
@@ -0,0 +1,3 @@
+## To run you local data in you localhost server use this command in terminal:
+`npx json-server -w _FOLDER/_FILE -p _PORT
+`
---
+++ LESSON-16/src/context/ProductsProvider.tsx
@@ -0,0 +1,62 @@
+import { ReactElement, createContext, useEffect, useState } from "react";
+
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
+};
+
+const initState: ProductType[] = [];
+
+// const initState: ProductType[] = [
+//   {
+//     sku: "item0001",
+//     name: "Widget",
+//     price: 9.99,
+//   },
+//   {
+//     sku: "item0002",
+//     name: "Premium Widget",
+//     price: 19.99,
+//   },
+//   {
+//     sku: "item0003",
+//     name: "Deluxe Widget",
+//     price: 29.99,
+//   },
+// ];
+
+export type UseProductContextType = { products: ProductType[] };
+
+const initContextState: UseProductContextType = { products: [] };
+
+const productsContext = createContext<UseProductContextType>(initContextState);
+
+type childrenType = { children: ReactElement | ReactElement[] };
+
+export const productsProvider = ({ children }: childrenType): ReactElement => {
+  const [products, setProducts] = useState<ProductType[]>(initState);
+
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch("http://localhost:3500/products")
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          if (err instanceof Error) console.log(err.message);
+        });
+      return data;
+    };
+
+    fetchProducts().then((products) => setProducts(products));
+  }, []);
+
+  return (
+    <productsContext.Provider value={{ products }}>
+      {children}
+    </productsContext.Provider>
+  );
+};
+
+export default productsContext;
